a1209b0b41999840c94d03eb72cb584c9a5f6776
Victoria Gonzales <vagp89@gmail.com>
2021-02-12 00:10:18 +0100

    I did the navbar

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <img className='header_logo' src="./amazon.png"/>
+        <div className='header_search'>
+        <input className = 'header_searchInput' type="text" />
+        <SearchIcon className='header_searchIcon'/>
+      </div>
+      <div className='header_nav'>
+        <div className='header_option'>
+          <span className='header_optionLineOne'>Hello guest</span>
+          <span className='header_optionLineTwo'>Sign In</span>
+        </div>
+        <div className='header_option'>
+          <span className='header_optionLineOne'>Returns</span>
+          <span className='header_optionLineTwo'>& Orders</span>
+        </div>
+        <div className='header_option'>
+          <span className='header_optionLineOne'>Your</span>
+          <span className='header_optionLineTwo'>Prime</span>
+        </div>
+        <div className='header_option'>
+          <AddShoppingCartIcon/>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBTJxIHRq2IRSwcgRIOj_ykGsOprlLpChw",
+  authDomain: "clone-e9537.firebaseapp.com",
+  projectId: "clone-e9537",
+  storageBucket: "clone-e9537.appspot.com",
+  messagingSenderId: "385131635286",
+  appId: "1:385131635286:web:d982e6e344a57e2c51d1f7",
+  measurementId: "G-8C4TZFBW21"
+};
