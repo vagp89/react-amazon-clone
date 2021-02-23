61b0fd6f5da819f292d16400f623b894b9271406
Victoria Gonzales <vagp89@gmail.com>
2021-02-23 23:34:02 +0100

    The last config changes in the backend

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instace = axios.create({
 
-  baseURL: "https://us-central1-clone-e9537.cloudfunctions.net/api"
+  baseURL: "https://us-central1-amzon-clone-vagp.cloudfunctions.net/api"
   //'http://localhost:5001/clone-e9537/us-central1/api' // The API (cloud fuction) URL
   })
 
